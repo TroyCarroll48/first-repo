04338a48fbc9020644088a1ec2c55547a9316f0c
TroyCarroll48 <troycarroll48@yahoo.com>
2018-09-12 13:24:40 -0400

    first pus h

---
+++ src/main/java/demopackage/Main.java
@@ -0,0 +1,167 @@
+package demopackage;
+
+import java.util.Scanner;
+
+import static java.lang.System.out;
+import static java.lang.System.setOut;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+//        String all = String.join(" / ", " Troy ", " carroll ", " Tsavage ");
+//        System.out.println(all);
+//        String greeting = "hello";
+//                greeting = "yes";
+//                System.out.println(greeting);
+//
+//        String x = "ill Nana";
+//        String y = "ill Nana";
+//        System.out.println(x.equals(y));
+//\n" +
+        Scanner input = new Scanner(System.in);
+//
+//        System.out.println("whats your name");
+//
+//        String name = input.nextLine();
+//
+//        System.out.println("So your name is " + name);
+//
+//        System.out.println("May i have your number");
+//
+//        long digits = input.nextLong();
+//
+//        System.out.println("Ahh so your number is " + digits);
+//
+//        System.out.println("Last thing, are you DTF?");
+//
+//        boolean ok = input.nextBoolean();
+
+//        if (ok == true) {
+//        System.out.println("ard lets get it popping");
+//        } else {
+//            System.out.println("damn thats upsetting");
+//        }
+
+//
+//        System.out.println(" give me one random numbers");
+//
+//        int num1 = input.nextInt();
+//
+//        System.out.println(num1);
+//
+//        System.out.println(" give me a second random number");
+//
+//        int num2 = input.nextInt();
+//
+////        long num3 = (num1 + num2);
+//
+//        long num4 = (num1 * num2);
+//
+//        long num5 = (num1 % num2);
+//
+//
+//
+//        System.out.println(" the sum of " + num1 + num2  + "is" + (num1 + num2));
+//        System.out.println(" the product of " + (num1) + "*" +  (num2) + "is" + (num1 * num2));
+//        System.out.println(" the remainder of " + (num1) + "%" + (num2) + "is" + (num1 % num2));
+
+//        String name = "Troy Carroll";
+//        int number = 78;
+//        long digit = 4000;
+//
+//        System.out.printf("%3d", -number);
+//        System.out.printf("%+25d", number);
+//        System.out.printf("%50s", name);
+//        System.out.printf("% 050d", digit);
+//
+//
+
+
+//        int x = 10;
+//
+//        while(x >= 0){
+//            System.out.println(" the value of x is " + x);
+//            x = x - 2;
+//        }
+//
+//        int i = 1;
+//        System.out.println("welcome to my website and enter your password");
+//
+//        do {
+//            System.out.println("Trial # " + i);
+//            System.out.println("try again");
+//            i++;
+//            if(i == 4){
+//                System.out.println("you are locked out");
+//
+//            }
+//        }while (i < 4);
+
+//                String name = "bob";
+//                System.out.println("Hello" +  name + "Have a nice day");
+//
+//        do {
+//            System.out.println("Trial # " + name);
+//            System.out.println("try again");
+//            System.out.println("you are locked out");
+//
+//        }while(name);
+
+
+//
+//        System.out.println("who are you?");
+//        String lol = input.nextLine();
+//        if(nextInput.equals("bob"));
+
+        String login2 =
+        String name1 = "bob";
+        System.out.println("who is trying to log into this computer");
+        String name = input.nextLine();
+        while(name1 == "bob"){
+            System.out.println("Welcome bob have a great day ");
+
+        }while(name)
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//           System.out.println("what is your name");
+
+
+//           if(name == "bob");
+//        {
+//            System.out.println("what is your name");
+//            String dood = input.nextLine();
+//           System.out.println("Welcome bob have a great day");
+//        }if
+//
+//        (name2 == "alice");{
+//            System.out.println("Welcome alice have a great day");
+//
+//        }if (name == null);
+//
+//        System.out.println("Please enter a valid name");
+
+
+
+    }
+}
---
+++ src/main/java/demopackage/Test.java
@@ -0,0 +1,10 @@
+package demopackage;
+
+public class Test {
+    public static void main(String[] args) {
+         int age = 20;
+         String name = "troy";
+
+        System.out.println("my age is " + age + "and my name is" + name);
+    }
+}
